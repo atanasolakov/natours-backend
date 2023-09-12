31d5978b483c7df644223a9f47260128d93107e3
Your Name <you@example.com>
2023-09-12 22:16:44 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const morgan = require('morgan');
+const userRouter = require('./routes/userRoutes')
+const tourRouter = require('./routes/tourRoutes')
+
+const app = express();
+
+if(process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+app.use(express.static(`${__dirname}/public`))
+app.use(express.json());
+app.use((req,res,next) => {
+  console.log('Hello from middleware 😁')
+  next()
+})
+
+app.use((req,res,next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+})
+
+app.use('/api/v1/users', userRouter);
+app.use('/api/v1/tours', tourRouter);
+
+module.exports = app;
