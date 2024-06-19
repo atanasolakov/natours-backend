065120da842ed3aeaa86d37be9015bf6d89dc3dc
aSolakov <asolakov@imm.dev>
2024-06-19 18:29:47 +0300

    error controllers

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const morgan = require('morgan');
-const userRouter = require('./routes/userRoutes')
-const tourRouter = require('./routes/tourRoutes')
+
+const AppError = require('./utils/appError');
+
+const globalErrorHandler = require('./controllers/errorController')
+const userRouter = require('./routes/userRoutes');
+const tourRouter = require('./routes/tourRoutes');
 
 const app = express();
 
@@ -12,16 +16,30 @@ if(process.env.NODE_ENV === 'development') {
 app.use(express.static(`${__dirname}/public`))
 app.use(express.json());
 app.use((req,res,next) => {
-  console.log('Hello from middleware 😁')
-  next()
-})
-
-app.use((req,res,next) => {
   req.requestTime = new Date().toISOString();
   next();
 })
 
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/tours', tourRouter);
+
+app.all('*', (req, res,next) => {
+
+
+  // res.status(404).json({
+  //   status: 'fail',
+  //   message: `Cant find ${req.originalUrl} on the server`
+  // })
+
+  // const err = new Error(`Cant find ${req.originalUrl} on the server`);
+  // err.status = 'fail';
+  // err.statusCode = 404;
+
+  // next(err);
+
+   next(new AppError(`Cant find ${req.originalUrl} on the server`), 404);
+});
+
+app.use(globalErrorHandler);
 
 module.exports = app;
